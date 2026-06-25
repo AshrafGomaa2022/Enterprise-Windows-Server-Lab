822d4c7f2c393bd312001daafefbbc8cff83a327
Ashraf Gomaa <106782584+AshrafGomaa2022@users.noreply.github.com>
2026-06-25 06:38:02 +0300

    Update 1-Create-OUs.ps1

--- PowerShell-Scripts/1-Create-OUs.ps1
+++ PowerShell-Scripts/1-Create-OUs.ps1
@@ -1,1 +1,14 @@
+# Script: Create-Users.ps1
+# Purpose: Create the HR user 'ashraf' in the HR OU
+Import-Module ActiveDirectory
+ $DomainDN = (Get-ADDomain).DistinguishedName
+ $OUPath = "OU=HR,$DomainDN"
+ $Password = ConvertTo-SecureString "P@ssw0rd123" -AsPlainText -Force
 
+ $Username = "ashraf"
+if (!(Get-ADUser -Filter {SamAccountName -eq $Username})) {
+    New-ADUser -Name $Username -SamAccountName $Username -UserPrincipalName "$Username@network.local" -Path $OUPath -AccountPassword $Password -Enabled $true
+    Write-Host "[+] User '$Username' created in HR OU." -ForegroundColor Green
+} else {
+    Write-Host "[-] User '$Username' already exists." -ForegroundColor Yellow
+}
